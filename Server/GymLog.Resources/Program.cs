ca0ec8450a750307f3516cf1d022f8ee05f8c628
piter.s3335 <piter.s3335@gmail.com>
2019-06-15 17:39:21 +0200

    Resources

---
+++ Server/GymLog.Resources/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace GymLog.Resources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pl = Resource.ResourceManager.GetResourceSet(CultureInfo.InvariantCulture, true, true);
+            var en = Resource.ResourceManager.GetResourceSet(CultureInfo.GetCultureInfo("en-US"), true, true);
+
+            var rootPath = GoUp(Environment.CurrentDirectory, 5);
+            var clientTranslationsPath = $@"{rootPath}\Client\src\assets\i18n\";
+
+            Save(pl, clientTranslationsPath, nameof(pl));
+            Save(en, clientTranslationsPath, nameof(en));
+        }
+
+        private static void Save(IEnumerable resourceSet, string clientTranslationsPath, string suffix)
+        {
+            var json = JsonString(resourceSet);
+            File.WriteAllText($"{clientTranslationsPath}{suffix}.json", json);
+        }
+
+        private static string JsonString(IEnumerable resourceSet)
+        {
+            var dictionary = resourceSet.Cast<DictionaryEntry>().ToDictionary(x => x.Key.ToString(), x => x.Value.ToString());
+            return JsonConvert.SerializeObject(dictionary, Formatting.Indented);
+        }
+
+        private static string GoUp(string path, int levels)
+        {
+            if (levels == 0)
+                return path;
+
+            var temp = Directory.GetParent(path).ToString();
+
+            return GoUp(temp, levels - 1);
+        }
+    }
+}
